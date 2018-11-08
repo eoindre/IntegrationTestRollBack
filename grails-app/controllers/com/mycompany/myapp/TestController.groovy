8addd2b95a8ffa4570e70eccb3b023b0ccfef5aa
gstewart <george.stewart@ecurrency.net>
2018-11-08 18:16:33 +0000

    Service Not Rolling Back

--- grails-app/controllers/com/mycompany/myapp/TestController.groovy
+++ grails-app/controllers/com/mycompany/myapp/TestController.groovy
@@ -2,6 +2,15 @@ package com.mycompany.myapp
 
 class TestController {
 
+    TestingService testingService
+
     def index() {
+        Factory factoryOne = new Factory(name: "first").save(flush: true)
+        Factory factoryTwo = new Factory(name: "second").save(flush: true)
+        List<Factory> factories = [factoryOne, factoryTwo]
+        testingService.testServiceMethod(factories)
+        println "First Factory: $factoryOne.testField"
+        println "First Factory: $factoryTwo.testField"
+        render 'Check Console'
     }
 }
