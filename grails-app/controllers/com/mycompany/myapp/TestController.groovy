ab1b2731b2ed278058e6a8b5e947fbf9b79c02c6
gstewart <george.stewart@ecurrency.net>
2018-11-08 16:44:57 +0000

    Integration Tests Not Rolling Back

--- grails-app/controllers/com/mycompany/myapp/TestController.groovy
+++ grails-app/controllers/com/mycompany/myapp/TestController.groovy
@@ -3,8 +3,5 @@ package com.mycompany.myapp
 class TestController {
 
     def index() {
-        Factory testFactory = Factory.findByName('Test Factory')
-        println "Primary Factory: $testFactory.primaryFactory.name"
-        render 'Check Console'
     }
 }
